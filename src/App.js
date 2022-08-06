94ce9ff420c144c3210c3ebbc4d8070f71e32c9a
rafay9475 <rafay9475@gmail.com>
2022-08-06 15:55:29 +0500

    Till Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 function App() {
+  const[mode,setMode]=useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey';
+  }
+  else{
+    setMode('light')
+    document.body.style.backgroundColor='white';
+  }
+}
   return (
     <>
-<Navbar />
-{/* <div className="container">
-<TextForm heading="Enter Your Text to Analyze"/>
-</div> */}
-<About/>
+<Navbar mode={mode} toggleMode={toggleMode}/>
+<div className="container">
+<TextForm heading="Enter Your Text to Analyze" mode={mode}/>
+</div>
+{/* <About/> */}
 
 </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,16 +21,17 @@ setText(event.target.value)
     const [text, setText] = useState("");
   return (
     <>
-    <div>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
       <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"/>
+  <textarea className="form-control" style={{backgroundColor:props.mode==='dark'?'grey':'white',
+color:props.mode==='dark'?'white':'black'}} value={text} onChange={handleOnChange} id="myBox" rows="8"/>
 </div>
 <button type="button" className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
 <button type="button" className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
 <button type="button" className="btn btn-danger mx-1" onClick={handleClear}>Clear Text</button>
     </div>
-    <div className='container my-3'>
+    <div className='container my-3' style={{color:props.mode==='dark'?'white':'black'}}>
         <h2>Your Text Summary</h2>
     <p>{text.split(' ').length} words and {text.length} characters</p>
     <p>Takes {0.008*text.split(' ').length} minutes to read</p>
